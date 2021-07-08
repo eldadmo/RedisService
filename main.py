20bed4d27c827ddf0f1a74f244efcedaa5a40b6a
Eldad <Eldad@ELDAD>
2021-07-08 14:45:41 +0300

    Add configuration for redis service to be able to change port and ip

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import json
+import os
+
 import messageController
 import serverProvider
+from serverConfig import ServerConfig
+
+
+def ReadServerConfig():
+    try:
+        cwd = os.getcwd()
+        environment_path = cwd
+        with open(environment_path + "/serverConfig.json") as jsonFile:
+            json_object = json.load(jsonFile)
+            server_config_object = ServerConfig(**json_object)
+            jsonFile.close()
+        return server_config_object
+    except Exception as ex:
+        print("Warning: Cannot parse serverConfig.json from path:" + str(
+            environment_path) + "/serverConfig.json\r\n" + "Exception:" + str(ex))
+        return None
+
 
 if __name__ == '__main__':
     serverProvider.Init()
-    messageController.api.run()
+    serverConfig = ReadServerConfig()
+    if serverConfig is None:
+        messageController.api.run()
+    else:
+        messageController.api.run(serverConfig.localhost, serverConfig.port)
---
+++ serverConfig.py
@@ -0,0 +1,4 @@
+class ServerConfig:
+    def __init__(self, localhost, port):
+        self.localhost = localhost
+        self.port = port
